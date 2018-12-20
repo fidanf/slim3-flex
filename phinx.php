1533467db00f8f01ffe341085e5da99a410c5500
Çağlar Karakoç <caglarkarakoc@hotmail.com.tr>
2018-12-20 16:44:33 +0300

    Added Phinx file

---
+++ phinx.php
@@ -0,0 +1,28 @@
+<?php 
+require __DIR__ . '/vendor/autoload.php';
+
+try {
+    (new Dotenv\Dotenv(__DIR__))->load();
+} catch (Dotenv\Exception\InvalidPathException $e) {
+    die($e);
+}
+
+$config = require __DIR__ . '/config/database.php';
+
+return [
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/database/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/database/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => $config['migrations'],
+        'default' => [
+          	'adapter' => $config['db']['driver'],
+            'host' => $config['db']['host'],
+            'name' => $config['db']['database'],
+            'user' => $config['db']['username'],
+            'pass' => $config['db']['password'],
+            'port' => $config['db']['port'],
+        ]
+    ]
+];
