65b677edff2a6fb31ee60dac53d8233e2f02948f
fidanzaEPSI <frankfid@wanadoo.fr>
2018-12-31 10:06:46 +0100

    Fixed column name

--- database/migrations/20181228103654_create_users_table.php
+++ database/migrations/20181228103654_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
     {
         $this->schema->create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
